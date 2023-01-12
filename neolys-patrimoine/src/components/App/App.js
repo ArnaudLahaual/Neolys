19bbe92a0ee479c6455bba7ddb9df8d1a699c118
ArnaudLahaual <arnaud.lahaual@gmail.com>
2023-01-12 15:55:15 +0100

    installation Sass

---
+++ neolys-patrimoine/src/components/App/App.js
@@ -0,0 +1,14 @@
+import './app.scss'
+import Header from '../Header/header';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ neolys-patrimoine/src/components/Header/header.js
@@ -0,0 +1,15 @@
+// == Import
+import './header.scss';
+
+// == Composant
+function Header() {
+  return (
+    <div className="header">
+        <h>yoyo</h>
+      
+    </div>
+  );
+}
+
+// == Export
+export default Header ;
