e6a5dd020571b506ea81be4b861daea12c5ab905
ArnaudLahaual <arnaud.lahaual@gmail.com>
2023-01-13 14:02:06 +0100

    première tentative navbarmobile

---
+++ neolys-patrimoine/src/components/Header/NavbarMobile/navbarmobile.js
@@ -0,0 +1,15 @@
+// == Import
+import './navbarmobile.scss';
+import { FiMenu } from 'react-icons/fi';
+
+// == Composant
+function NavbarMobile() {
+  return (
+    <div className="navbarmobile">
+      <FiMenu size={40} />
+    </div>
+  );
+}
+
+// == Export
+export default NavbarMobile;
--- neolys-patrimoine/src/components/Header/header.js
+++ neolys-patrimoine/src/components/Header/header.js
@@ -1,20 +1,23 @@
 // == Import
 import './header.scss';
 import Navbar from './Navbar/navbar';
+import NavbarMobile from './NavbarMobile/navbarmobile';
 import logo from '../../assets/logo/logo__white.jpg';
 
 // == Composant
 function Header() {
   return (
     <div className="header">
-           
-           <img
-          className="header__logoNeolys"
-          src={logo}
-          alt="logoNeolys"
-        />
-
+           <div className='logo'>
+              <img
+              className="header__logoNeolys"
+              src={logo}
+              alt="logoNeolys"
+            />
+          </div>
+          
         <Navbar />
+        <NavbarMobile />
       
     </div>
   );
