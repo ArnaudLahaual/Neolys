baea7389b377dcf298c38aecdad1473dfa229047
ArnaudLahaual <arnaud.lahaual@gmail.com>
2023-01-13 14:21:28 +0100

    ok avant config

--- neolys-patrimoine/src/components/Header/header.js
+++ neolys-patrimoine/src/components/Header/header.js
@@ -5,9 +5,19 @@ import NavbarMobile from './NavbarMobile/navbarmobile';
 import logo from '../../assets/logo/logo__white.jpg';
 
 import { FiMenu } from 'react-icons/fi';
+import { toggleBurger, closeBurger } from '../../actions/neolys';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useLocation } from 'react-router-dom';
+// import { useEffect } from 'react';
 
 // == Composant
 function Header() {
+
+  // const dispatch = useDispatch();
+  // const { pathname } = useLocation();
+
+  
+
   return (
     <div className="header">
            <div className='logo'>
