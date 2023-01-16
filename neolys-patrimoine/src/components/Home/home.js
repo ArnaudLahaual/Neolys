53e0ba7cf2f73694fa81ff786815dd3edbd50878
Arnaud <arnaud.lahaual@gmail.com>
2023-01-16 16:17:10 +0100

    mise en place image principale et texte de présentation

--- neolys-patrimoine/src/components/Home/home.js
+++ neolys-patrimoine/src/components/Home/home.js
@@ -5,13 +5,19 @@ function Home() {
   return (
     <div className="home">
 
-<div className='decor'>
+        <div className='decor'>
               <img
               className="decor__home"
               src={decor}
               alt="decor__home"
             />
-          </div>
+        </div>
+
+        <div className='decor__text'>
+            <p>Exigez le meilleur pour vos investissements</p>
+        </div>
+
+        
     </div>
   );
 }
