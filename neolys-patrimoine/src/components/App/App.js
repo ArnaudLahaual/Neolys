cbf7d26440645428ffe431aa29c6a975620913d9
Arnaud <arnaud.lahaual@gmail.com>
2023-01-18 14:13:23 +0100

    slide mis en place, a rectificer car doublon image

--- neolys-patrimoine/src/components/App/App.js
+++ neolys-patrimoine/src/components/App/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Home from '../Home/home';
 import Team from '../Home/Team/team';
 import Services from '../Home/Services/services';
+import SlideShow from '../Home/Slideshow/slideshow';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Home />
         <Team />
         <Services />
+        <SlideShow />
     </div>
   );
 }
---
+++ neolys-patrimoine/src/components/Home/Slideshow/slideshow.js
@@ -0,0 +1,42 @@
+import './slideshow.scss';
+import React from 'react';
+import {Slide} from 'react-slideshow-image';
+import cyril from '../../../assets/images/cyril.png';
+import boris from '../../../assets/images/boris.png';
+
+const proprietes = {
+    duration : 5000,
+    transitionDuration :700,
+    infinite: true,
+    indicators: false,
+    arrows : false,
+ }
+
+function SlideShow() {
+  return (
+
+        <div className='containerSlide'>
+
+            <div className='containerSlide__title'>
+                <h1>Nos Partenaires</h1>
+            </div>
+        
+        <Slide {...proprietes}>
+            <div className='each-slide'>
+                <div className='images__partenaires'>
+                    <img src={cyril} alt='cyril'id='cyril' />
+                </div>
+            </div>
+            
+            <div className='each-slide'>
+                <div className='images__partenaires' >
+                    <img src={boris} alt='boris'id='boris' />
+                </div>
+            </div>
+        </Slide>
+        
+        </div>
+  );
+}
+
+export default SlideShow;
